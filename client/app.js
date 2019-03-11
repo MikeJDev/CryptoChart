1bbd556d370efaed28a45409f3f6604f5404816f
MikeJDev <ukmjanes@gmail.com>
2019-03-11 12:16:19 -0500

    created new component for a chart, as well as a route to coindexk apu endpoint

--- client/app.js
+++ client/app.js
@@ -1,11 +1,45 @@
 import React from "react";
+import axios from "axios";
+import NewChart from "./chart.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    };
   }
+
+  getPrice() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/historical/close.json")
+      .then(response => {
+        console.log(response.data.bpi);
+        this.setState({
+          data: response.data.bpi
+        });
+      })
+      .catch(err => {
+        return err;
+      });
+  }
+
   render() {
-    return <h1>Whats going on, manhghghg?</h1>;
+    return (
+      <div>
+        <button type="button" onClick={this.getPrice}>
+          {" "}
+          Get Current Prices{" "}
+        </button>
+        <NewChart />
+      </div>
+    );
   }
 }
 export default App;
+
+/*
+start = year, month, day
+end = year, month, day
+
+*/
